5c5428cad31629f1666208e911013fffceaf131a
Placeholder211 <235861@edu.p.lodz.pl>
2021-02-05 23:43:36 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ou_lab_github.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ou_lab_github.dir/main.cpp.o"
+  "ou_lab_github"
+  "ou_lab_github.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ou_lab_github.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/ou_lab_github.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for ou_lab_github.
+# This may be replaced when dependencies are built.
